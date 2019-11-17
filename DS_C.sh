af3274b0db40faec0181fd50a518783755bd3807
Efan Andrian <efan.andrian05@gmail.com>
2019-11-17 22:55:38 +0700

    se

---
+++ DS_C.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+"C:\ds_merahputih\kill_DS.exe" $1 &
+git pull
+"C:\ds_merahputih\DesktopService.exe" $1 &
+
+
---
+++ DS_D.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+"D:\ds_merahputih\kill_DS.exe" $1 &
+git pull
+"D:\ds_merahputih\DesktopService.exe" $1 &
+
